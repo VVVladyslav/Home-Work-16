471ca49bd52fd9f4e61d9bd78b700df7ce9cf083
VVVladyslav <vladoszhar@gmail.com>
2024-03-26 18:28:24 +0200

    a

---
+++ src/main/java/com/example/Today/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.Today;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/api/v1/auth/getUserInfo").permitAll()
+                                .requestMatchers("/lang/api/**").permitAll()
+                                .requestMatchers("/lang/list").hasAuthority("ROLE_ADMIN")
+                                .anyRequest().authenticated())
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+    // Define the PasswordEncoder bean
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Configure the UserDetails service for JDBC authentication
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery("select username, password, enabled from users where username=?");
+        userDetailsManager.setAuthoritiesByUsernameQuery("select username, role from users where username=?");
+        return userDetailsManager;
+    }
+}
