535ca7d4d4f986280c02af84fa640fc94f37ab2b
Vladyslav Zharkov <138609115+VVVladyslav@users.noreply.github.com>
2024-04-25 14:38:00 +0300

    Update NoteController.java

--- src/main/java/com/example/Today/NoteController.java
+++ src/main/java/com/example/Today/NoteController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @Controller
 @RequestMapping("/note")
-//@RequiredArgsConstructor
 public class NoteController {
     private final NoteService noteService;
     @Autowired
