41cdc0a02b00e30d0e8d87c8a634683f663044bf
Vladyslav Zharkov <138609115+VVVladyslav@users.noreply.github.com>
2024-04-25 14:38:20 +0300

    Update NoteService.java

--- src/main/java/com/example/Today/CRUD/NoteService.java
+++ src/main/java/com/example/Today/CRUD/NoteService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
-//@RequiredArgsConstructor
 public class NoteService {
 
     private final NoteRepository noteRepository;
